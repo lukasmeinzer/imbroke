ed79c88b6fac154faa405b42ad8c98d075450f84
lukasmeinzer <lukas.meinzer.98@web.de>
2023-09-17 20:26:05 +0200

    Config -> Toml

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 """
 TODO fürs nächste Mal:
-- angebote_dict (Kaufland) schön in DF formatiern und in DB speichern
-    - dafür muss die "hochladen" Funktion noch angepasst und verbessert werden 
-        (dass nur DFs hochgeladen werden können)
-- Scraping_Input.toml erstellen und da die ganzen Variablen reinpacken
 - DataBase in eigenen DB Ordner packen und dann jeweils eine DB File für Connection
 - Für Apis evtl je Markt eine eigene DB File
 - Dann alles für Edeka nach dem exakt gleichem Schema machen
 - DB_Konfiguration.toml ebenso erstellen
 """
 
+import toml
 
 import Maerkte.Kaufland as Kaufland
-import Database
 
 
-Kaufland.AllKaufland(url='https://filiale.kaufland.de/angebote/aktuelle-woche')
+config = toml.load('Crawler_Input.toml')
+
+kaufland_url = config['Kaufland']['url']
+Kaufland.AllKaufland(kaufland_url)
