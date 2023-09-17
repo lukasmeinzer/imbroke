bed3e9451de5fc80f6495a9df295e9818261af83
lukasmeinzer <lukas.meinzer.98@web.de>
2023-09-17 12:04:51 +0200

    TODOs angepasst

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """
 TODO fürs nächste Mal:
-- angebote_dict schön in DF formatiern und in DB speichern
-- Dann Kaufland scrapen und diese Infos in DB speichern 
-- Dann alles für Edeka nach dem exakt gleichem Schema machen
+- angebote_dict (Kaufland) schön in DF formatiern und in DB speichern
+    - dafür muss die "hochladen" Funktion noch angepasst und verbessert werden 
+        (dass nur DFs hochgeladen werden können)
 - Scraping_Input.toml erstellen und da die ganzen Variablen reinpacken
+- DataBase in eigenen DB Ordner packen und dann jeweils eine DB File für Connection
+- Für Apis evtl je Markt eine eigene DB File
+- Dann alles für Edeka nach dem exakt gleichem Schema machen
 - DB_Konfiguration.toml ebenso erstellen
 """
 
